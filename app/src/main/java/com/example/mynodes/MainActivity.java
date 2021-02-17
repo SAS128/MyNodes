247d708246d5379192346c774cd11ca53a1ffe60
Student <Sandmen461@gmail.com>
2021-02-17 18:58:33 +0200

    Add CustomDialogFragment

--- app/src/main/java/com/example/mynodes/MainActivity.java
+++ app/src/main/java/com/example/mynodes/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.mynodes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void showMyDialog(View v){
+        CustomDialogFragment fragment = new CustomDialogFragment();
+        fragment.show(getSupportFragmentManager(), "custom");
     }
 }
