d95f855292e70383f5d9bd4604648111bfa9eb97
Student <Sandmen461@gmail.com>
2021-02-17 20:49:23 +0200

    Fix bug

--- app/src/main/java/com/example/mynodes/MainActivity.java
+++ app/src/main/java/com/example/mynodes/MainActivity.java
@@ -17,18 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView v = (ListView)findViewById(R.id.useritems);
-
-        XmlPullParser xpp = getResources().getXml(R.xml.users);
-        UserResourceParser parser = new UserResourceParser();
-        if(parser.parse(xpp)){
-            for(User user : parser.getUsers()) {
-                Log.d("XML", user.toString());
-            }
-
-            ArrayAdapter<User> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, parser.getUsers());
-            v.setAdapter(adapter);
-        }
+//        ListView v = (ListView)findViewById(R.id.useritems);
+//
+//        XmlPullParser xpp = getResources().getXml(R.xml.users);
+//        UserResourceParser parser = new UserResourceParser();
+//        if(parser.parse(xpp)){
+//            for(User user : parser.getUsers()) {
+//                Log.d("XML", user.toString());
+//            }
+//
+//            ArrayAdapter<User> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.support_simple_spinner_dropdown_item, parser.getUsers());
+//            v.setAdapter(adapter);
+//        }
     }
     public void showMyDialog(View v){
         CustomDialogFragment fragment = new CustomDialogFragment();
